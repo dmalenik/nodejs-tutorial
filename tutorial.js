9ae4a3080e12fbf09cf715bcf0b7eff350ae3a02
Dmytro Malienik <dmalenik@gmail.com>
2023-09-05 20:25:09 +0300

    feat(tutorial.js): create the object for export purposes

---
+++ tutorial.js
@@ -0,0 +1,11 @@
+const sum = (num1, num2) => num1 + num2;
+
+class SomeMathObj {
+    constructor() {
+        console.log("Object created");
+    }
+}
+
+const PI = 3.14;
+
+export {sum, SomeMathObj, PI};
