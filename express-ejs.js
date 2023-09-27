570e90c359cd5643e601fe6248578fddadaee2f6
Dmytro Malienik <dmalenik@gmail.com>
2023-09-27 16:48:18 +0300

    feat: pass dynamic management of content with ejs
    - install dependencies
    - create index.ejs file with a content
    - set view engine as ejs
    - handle get request

---
+++ express-ejs.js
@@ -0,0 +1,36 @@
+import express from "express";
+import { fileURLToPath } from "node:url";
+import { join, dirname } from "node:path";
+
+const __filename = fileURLToPath(import.meta.url); // returns a path to the current file
+const __dirname = dirname(__filename); // returns a path to the directory of the current file
+
+const app = express();
+
+// middleware
+// set aliases of static folder to be public
+app.use(express.static(`${__dirname}/static`));
+
+// set ejs
+// create views folder
+app.set("view engine", "ejs");
+
+// get into files
+// the server will look inside ejs file, render it, and return HTML file to the client
+app.get("/:userQuery", (req, res) => {
+  // render data
+  res.render("index", {
+    data: {
+      userQuery: req.params.userQuery,
+      searchResults: ["book1", "book2", "book3"],
+      loggedIn: true,
+      username: "Dmitriy",
+    },
+  });
+});
+
+app.get("/example", (req, res) => {
+  res.send("hitting example route!");
+});
+
+app.listen(3000);
