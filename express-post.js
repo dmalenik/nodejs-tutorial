0f150d3ef89345c6729504f9d61db6016db911f4
Dmytro Malienik <dmalenik@gmail.com>
2023-09-27 16:44:29 +0300

    feat: pass handling post requests with express
    - install dependencies
    - create body parses and pass it in the middleware

---
+++ express-post.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { fileURLToPath } from "node:url";
+import { join, dirname } from "node:path";
+import bodyParser from "body-parser";
+
+const __filename = fileURLToPath(import.meta.url); // returns a path to the current file
+const __dirname = dirname(__filename); // returns a path to the directory of the current file
+
+const app = express();
+
+// middleware
+// set aliases of static folder to be public
+app.use(express.static(`${__dirname}/static`));
+// use body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+// // get into files
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+});
+
+app.get('/example', (req, res) => {
+    res.send("hitting example route!");
+});
+
+// code a POST response
+app.post('/', (req, res) => {
+    console.log(req.body);
+    // database work here
+    res.send("successfully posted data");
+});
+
+app.listen(3000);
